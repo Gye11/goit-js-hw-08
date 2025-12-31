c503c5456b63b69692e2f252609c7213e42bd80f
Gye11 <hilallornekk@gmail.com>
2026-01-01 01:26:37 +0300

    final

---
+++ js/gallery-items.js
@@ -0,0 +1,23 @@
+export const images = [
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2017/02/16/10/41/sea-2076840__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2017/02/16/10/41/sea-2076840_1280.jpg",
+    description: "Sea Beach",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2016/02/19/11/53/mountains-1207952__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2016/02/19/11/53/mountains-1207952_1280.jpg",
+    description: "Mountain View",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg",
+    description: "Lonely Tree",
+  },
+];
